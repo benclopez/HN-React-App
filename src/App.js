f163700ab2c12eace69c3945f57d54e24bd477a2
Ben Lopez <benlopez@mail.usf.edu>
2019-07-07 21:13:37 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.updateSearch = this.updateSearch.bind(this);
+    this.state = {
+      items: [],
+      next: 10,
+      start: 0,
+      search: '',
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://hacker-news.firebaseio.com/v0/beststories.json')
+      .then(response => response.json())
+      .then((data) => {
+        data.map((newsId) => {
+          fetch(`https://hacker-news.firebaseio.com/v0/item/${newsId}.json`)
+            .then(response => response.json())
+            .then((itemDetail) => {
+              this.setState((currentState) => {
+                currentState.items.push(itemDetail);
+                return { items: currentState.items };
+              })
+            })
+        });
+      })
+  }
+
+  updateSearch(event) {
+    console.log(event);
+    this.setState({
+      search: event.target.value.toLowerCase()
+    });
+  }
+
+  render() {
+    var { items } = this.state;
+    let filteredItems = items.filter(
+      (item) => {
+        return (item.title.toLowerCase().indexOf(this.state.search) !== -1 || item.by.toLowerCase().indexOf(this.state.search) !== -1)
+      }
+    );
+
+    items = filteredItems.map(item => {
+      return <li key={item.id}><a href={item.url} target="_blank" rel="noopener noreferrer">{item.title} - {item.by}</a></li>
+    });
+
+    return (
+      <div className="App">
+        <div id="topBar">
+          <h1>Best of Hacker News</h1>
+        </div>
+        <div>
+          <label>Search by Author or Title: </label>
+          <input id="search" type="text"
+            value={this.state.search}
+            onChange={this.updateSearch}></input>
+        </div>
+        <ul>
+          {items}
+        </ul>
+      </div>
+
+    );
+  }
 }
 
 export default App;
